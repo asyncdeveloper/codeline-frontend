15c33325fec6db948a587d4ff4e1a51c1c2ec877
Sheyilaaw <sheyilaaw98@gmail.com>
2018-10-31 23:45:37 +0100

    Added route for home and weather

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import Weather from "./components/Weather/Weather";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Switch>
+            <Route exact={true} path="/" component={App} />
+            <Route exact={true} path="/weather/:woeid" component={Weather} />
+        </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
