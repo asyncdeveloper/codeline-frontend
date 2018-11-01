a9db5d963d06794085ba8805015053d5bf357d5f
Sheyilaaw <sheyilaaw98@gmail.com>
2018-11-01 13:18:55 +0100

    added newly added components to app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ import './index.css';
 import App from "./App";
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Weather from "./components/Weather/Weather";
+import Weather from "./components/Weather";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 ReactDOM.render(
     <BrowserRouter>
-        <Switch>
-            <Route exact={true} path="/" component={App} />
-            <Route exact={true} path="/weather/:woeid" component={Weather} />
-        </Switch>
+        <div>
+            <Header />
+            <Switch>
+                <Route exact={true} path="/" component={App} />
+                <Route exact={true} path="/weather/:woeid" component={Weather} />
+                <Route exact={true} path="/search/:location" component={Search} />
+            </Switch>
+        </div>
     </BrowserRouter>,
     document.getElementById('root')
 );
