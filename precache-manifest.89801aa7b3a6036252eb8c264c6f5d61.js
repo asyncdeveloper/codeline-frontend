2079606520621a54f2b4171f02b36e149984e20a
Sheyilaaw <sheyilaaw98@gmail.com>
2019-04-15 17:44:44 +0100

    Updates

---
+++ precache-manifest.89801aa7b3a6036252eb8c264c6f5d61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c8da33c588866062c75",
+    "url": "/codeline-frontend/static/js/runtime~main.5c8da33c.js"
+  },
+  {
+    "revision": "77df0a107a3083844405",
+    "url": "/codeline-frontend/static/js/main.77df0a10.chunk.js"
+  },
+  {
+    "revision": "3e319883212fe62edda3",
+    "url": "/codeline-frontend/static/js/1.3e319883.chunk.js"
+  },
+  {
+    "revision": "77df0a107a3083844405",
+    "url": "/codeline-frontend/static/css/main.f11b3c55.chunk.css"
+  },
+  {
+    "revision": "06213f85593077b8daa3ef592ad19ff6",
+    "url": "/codeline-frontend/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da0f4107cee8793f23a1a22b2da0d674.js"
+  "/codeline-frontend/precache-manifest.89801aa7b3a6036252eb8c264c6f5d61.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/codeline-frontend/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
