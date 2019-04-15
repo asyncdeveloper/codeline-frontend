2f44129beec11a0fd4a5322b247e205c6fbe6211
Sheyilaaw <sheyilaaw98@gmail.com>
2019-04-15 17:35:45 +0100

    Updates

---
+++ precache-manifest.da0f4107cee8793f23a1a22b2da0d674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "21e7fcc832ebfade5773",
+    "url": "/static/js/main.21e7fcc8.chunk.js"
+  },
+  {
+    "revision": "3e319883212fe62edda3",
+    "url": "/static/js/1.3e319883.chunk.js"
+  },
+  {
+    "revision": "21e7fcc832ebfade5773",
+    "url": "/static/css/main.f11b3c55.chunk.css"
+  },
+  {
+    "revision": "1ddbdd18b1ad359c7bd407d4893e3a4c",
+    "url": "/index.html"
+  }
+];
